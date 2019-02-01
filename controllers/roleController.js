61c05044bc818c9b73dda699a8eb21cd5bf3671a
reuel88 <reuel.teodoro@gmail.com>
2019-02-01 17:02:45 +1100

    roleController - Reuel

---
+++ controllers/roleController.js
@@ -0,0 +1,77 @@
+const RoleModel = require('../database/models').role;
+const UserModel = require('../database/models').user;
+
+module.exports = {
+    // Get
+    list(req, res) {
+        return RoleModel
+            .findAll({
+                include: [{
+                    model: UserModel,
+                    as: 'users'
+                }]
+            })
+            .then(roles => res.status(200).send(roles))
+            .catch(error => res.status(400).send(error));
+    },
+    // Get
+    getById(req, res) {
+        return RoleModel
+            .findById(req.params.id, {
+                include: [{
+                    model: UserModel,
+                    as: 'users'
+                }]
+            })
+            .then(role => {
+                if (!role) return res.status(404).send({message: 'Role Not Found'});
+
+                return res.status(200).send(role);
+            })
+            .catch(error => res.status(400).send(error));
+
+    },
+    // Post
+    add(req, res){
+        return RoleModel
+            .create({roleName: req.body.roleName})
+            .then(role => res.status(201).send(role))
+            .catch(error => res.status(400).send(error));
+    },
+    // Update
+    update(req, res){
+        return RoleModel
+            .findById(req.params.id, {
+                include: [{
+                    model: UserModel,
+                    as: 'users'
+                }]
+            })
+            .then(role => {
+                if (!role) return res.status(404).send({message: 'Role Not Found'});
+
+                return role
+                    .update({
+                        roleName: req.body.roleName
+                    })
+                    .then(() => res.status(200).send(role))
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    },
+    // Delete
+    delete(req, res){
+        return RoleModel
+            .findById(req.params.id)
+            .then(role => {
+                if (!role) return res.status(404).send({message: 'Role Not Found'});
+
+                return role
+                    .destroy()
+                    .then(() => res.status(204).send(role))
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    }
+
+};
