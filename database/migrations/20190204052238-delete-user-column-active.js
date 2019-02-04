c588eca28f92d9397f7c260f36c03ed4a1579f9d
reuel88 <reuel.teodoro@gmail.com>
2019-02-04 16:36:06 +1100

    Fix up the model - Reuel

--- database/migrations/20190204052238-delete-user-column-active.js
+++ database/migrations/20190204052238-delete-user-column-active.js
@@ -1,26 +1,26 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface) => {
-      return queryInterface.removeColumn('users', 'active');
-  },
+    up: (queryInterface) => {
+        return queryInterface.removeColumn('users', 'active');
+    },
 
-  down: (queryInterface, Sequelize) => {
-      return queryInterface
-          .addColumn('users', 'active', {
-              type:Sequelize.BOOLEAN,
-              allowNull: false,
-              defaultValue: true
-          })
-          .then(() => {
-              queryInterface.sequelize.query(`
+    down: (queryInterface, Sequelize) => {
+        return queryInterface
+            .addColumn('users', 'active', {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: true
+            })
+            .then(() => {
+                queryInterface.sequelize.query(`
                     UPDATE users 
                     SET active = CASE
                         WHEN status='active' THEN 1
                         ELSE 0
                     END;
                 `);
-          });
+            });
 
-  }
+    }
 };
