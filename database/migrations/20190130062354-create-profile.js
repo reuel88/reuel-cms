5dab04ad16b1ee4d57cf6b79d128801e931eb537
reuel88 <reuel.teodoro@gmail.com>
2019-02-04 16:43:09 +1100

    Fix some migration scripts - Reuel

--- database/migrations/20190130062354-create-profile.js
+++ database/migrations/20190130062354-create-profile.js
@@ -1,36 +1,37 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('profiles', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      fullName: {
-        type: Sequelize.STRING
-      },
-      birthDate: {
-        type: Sequelize.DATE
-      },
-      gender: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface) => {
-    return queryInterface.dropTable('profiles');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('profiles', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            fullName: {
+                type: Sequelize.STRING
+            },
+            birthDate: {
+                type: Sequelize.DATE
+            },
+            gender: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface) => {
+        return queryInterface.dropTable('profiles');
+    }
 };
--- database/migrations/20190201052511-create-user-role.js
+++ database/migrations/20190201052511-create-user-role.js
@@ -1,30 +1,31 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('userRoles', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      roleId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface) => {
-    return queryInterface.dropTable('userRoles');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('userRoles', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            roleId: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface) => {
+        return queryInterface.dropTable('userRoles');
+    }
 };
