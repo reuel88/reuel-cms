7666609c9d2affc26e5085978d92f1677543c363
reuel88 <reuel.teodoro@gmail.com>
2019-02-04 16:25:56 +1100

    Added status user table - Reuel

--- database/migrations/20190203204434-update-user-column-active.js
+++ database/migrations/20190203204434-update-user-column-active.js
@@ -2,24 +2,25 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        // return Promise.all([
-        //     queryInterface.addColumn('users', 'status', {
-        //         type: Sequelize.STRING,
-        //         allowNull: false
-        //     }),
-        //     queryInterface.removeColumn('users', 'active')
-        // ]);
+        return queryInterface
+            .addColumn('users', 'status', {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: 'active'
+            })
+            .then(() => {
+                queryInterface.sequelize.query(`
+                    UPDATE users 
+                    SET status = CASE
+                        WHEN active=1 THEN 'active'
+                        ELSE 'inactive'
+                    END;
+                `);
+            });
 
     },
 
-    down: (queryInterface, Sequelize) => {
-        // return Promise.all([
-        //     queryInterface.addColumn('users', 'active', {
-        //         type:Sequelize.BOOLEAN,
-        //         allowNull: false,
-        //         defaultValue: true
-        //     }),
-        //     queryInterface.removeColumn('users', 'status')
-        // ]);
+    down: (queryInterface) => {
+        return queryInterface.removeColumn('users', 'status');
     }
 };
---
+++ database/migrations/20190204052238-delete-user-column-active.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface) => {
+      return queryInterface.removeColumn('users', 'active');
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface
+          .addColumn('users', 'active', {
+              type:Sequelize.BOOLEAN,
+              allowNull: false,
+              defaultValue: true
+          })
+          .then(() => {
+              queryInterface.sequelize.query(`
+                    UPDATE users 
+                    SET active = CASE
+                        WHEN status='active' THEN 1
+                        ELSE 0
+                    END;
+                `);
+          });
+
+  }
+};
