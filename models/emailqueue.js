8c56ab46a88cf13d35f7ac90ffb9b156a97c6bad
reuel88 <reuel.teodoro@gmail.com>
2019-02-06 16:48:44 +1100

    Update to email queue - Reuel

--- models/emailqueue.js
+++ models/emailqueue.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const mail = sequelize.define('email', {
+  const mail = sequelize.define('emailQueue', {
     from: DataTypes.STRING,
     to: DataTypes.STRING,
     cc: DataTypes.STRING,
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
     subject: DataTypes.STRING,
     text: DataTypes.STRING,
     html: DataTypes.STRING,
-    attachments: DataTypes.STRING
+    attachments: DataTypes.STRING,
+    status: DataTypes.STRING,
   }, {});
   mail.associate = function(models) {
     // associations can be defined here
