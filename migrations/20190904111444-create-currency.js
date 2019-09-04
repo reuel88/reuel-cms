bc59bbc02647607ead004d9fc6c8daca756b6c26
reuel88 <githubMagic1!>
2019-09-04 21:15:57 +1000

    Added Currency

---
+++ migrations/20190904111444-create-currency.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('currencies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      symbol: {
+        type: Sequelize.STRING
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      number: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('currencies');
+  }
+};
---
+++ models/currency.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const currency = sequelize.define('currency', {
+    country: DataTypes.STRING,
+    name: DataTypes.STRING,
+    symbol: DataTypes.STRING,
+    code: DataTypes.STRING,
+    number: DataTypes.INTEGER
+  }, {});
+  currency.associate = function(models) {
+    // associations can be defined here
+  };
+  return currency;
+};
