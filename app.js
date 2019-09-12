a77d55b543ab34b523dc65d1c876d62943be5b8f
reuel88 <githubMagic1!>
2019-09-12 10:56:24 +1000

    res headers

--- app.js
+++ app.js
@@ -28,6 +28,25 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use( function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', req.app.get('env') === 'development' ? '*' : 'http://localhost:3001');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/api/v1', authController.authenticateToken, apiRouter);
 
